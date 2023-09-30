60cde9953769d8bbdd7cdf17c437799f8741fbe6
sael-69 <145992575+sael-69@users.noreply.github.com>
2023-09-30 19:15:21 -0300

    Delete exceptions directory

--- exceptions/ExcecaoPorCoordenadaInvalida.java
+++
@@ -1,7 +0,0 @@
-package oo.jogodavelha.exceptions;
-
-public class ExcecaoPorCoordenadaInvalida extends Exception {
-    public ExcecaoPorCoordenadaInvalida(String mensagem) {
-        super(mensagem);
-    }
-}
--- exceptions/ExcecaoPorSimboloInvalido.java
+++
@@ -1,8 +0,0 @@
-package oo.jogodavelha.exceptions;
-
-public class ExcecaoPorSimboloInvalido extends Exception {
-    public ExcecaoPorSimboloInvalido(String mensagem) {
-        super(mensagem);
-    }
-}
-
