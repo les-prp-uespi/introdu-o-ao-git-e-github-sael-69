5134ea7ac3827f08e6ca1df807871c553b6bce34
sael-69 <145992575+sael-69@users.noreply.github.com>
2023-09-30 19:14:23 -0300

    Add files via upload

---
+++ exceptions/ExcecaoPorSimboloInvalido.java
@@ -0,0 +1,8 @@
+package oo.jogodavelha.exceptions;
+
+public class ExcecaoPorSimboloInvalido extends Exception {
+    public ExcecaoPorSimboloInvalido(String mensagem) {
+        super(mensagem);
+    }
+}
+
